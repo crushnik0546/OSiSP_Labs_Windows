58a8ebf9a6b6023b352943bde1900134dc708cd2
Катя Крашник <kotova123451234512345@gmail.com>
2021-11-14 17:32:43 +0300

    fix input dynamic link

--- Lab_3_C/DynamicLink/DynamicLink.cpp
+++ Lab_3_C/DynamicLink/DynamicLink.cpp
@@ -4,18 +4,28 @@ typedef int __cdecl DLLPROC(param_info* params);
 
 int main()
 {
-    char find[] = "abcd";
-    char replace[] = "zxcv";
+    char find[100];
+    char replace[100];
+
+    printf("What string I should find : ");
+    fgets(find, sizeof(find), stdin);
+    printf("How I should change this string (the same length): ");
+    fgets(replace, sizeof(replace), stdin);
 
     int str_len = strlen(find);
 
     char *find_str = (char *)malloc(sizeof(char) * str_len);
 
     if (NULL == find_str)
+    {
+        printf("Insufficient memory available\n");
         return -1;
+    }
 
     memcpy(find_str, find, str_len);
     find_str[str_len] = 0;
+
+    printf("String before dll call: %s\n", find_str);
 
     HINSTANCE lib;
     DLLPROC *string_replace;
@@ -25,6 +35,7 @@ int main()
 
     if (NULL == lib)
     {
+        printf("Fail Load library\n");
         return -1;
     }
 
@@ -32,10 +43,9 @@ int main()
 
     if (NULL == string_replace)
     {
+        printf("Fail Get process adress in user library\n");
         return -1;
     }
-
-    printf("String before dll call: %s\n", find);
 
     param_info params;
     params.pid = GetCurrentProcessId();
