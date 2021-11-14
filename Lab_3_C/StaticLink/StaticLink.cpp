3da90784b22e0e7d646c6cc32c34ff01e8f01ee5
Катя Крашник <kotova123451234512345@gmail.com>
2021-11-14 17:27:49 +0300

    fix input Statick link

--- Lab_3_C/StaticLink/StaticLink.cpp
+++ Lab_3_C/StaticLink/StaticLink.cpp
@@ -1,21 +1,24 @@
-﻿#include <stdlib.h>
-#include <stdio.h>
-#include <Windows.h>
-#include "replace_string.h"
+﻿#include "replace_string.h"
 
 int main()
 {
-    char find[] = "12345";
-    char replace[] = "67890";
+    char find[100];
+    char replace[100];
 
-    //printf("Adrees find_string: %X\n", find_str);
+    printf("What string I should find : ");
+    fgets(find, sizeof(find), stdin);
+    printf("How I should change this string (the same length): ");
+    fgets(replace, sizeof(replace), stdin);
 
     int str_len = strlen(find);
 
     char *find_str = (char *)malloc(sizeof(char) * str_len);
 
     if (NULL == find_str)
+    {
+        printf("Insufficient memory available\n");
         return -1;
+    }
 
     memcpy(find_str, find, str_len);
     find_str[str_len] = 0;
@@ -27,12 +30,15 @@ int main()
     params.find_string = find_str;
     params.replace_string = replace;
 
-    int k = replace_string(&params);
-
-    printf("String after dll call: %s\n", find_str);
-
-    //free(find_str);
-    //free(replace_str);
+    if (!replace_string(&params))
+    {
+        printf("String was changed successfully!\n");
+        printf("String after dll call: %s\n", find_str);
+    }
+    else
+    {
+        printf("String was not changed. DLL error");
+    }
 
     return 0;
 }
